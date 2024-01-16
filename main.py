1da939a665ed4cf3a7081e5386856f4bf519b767
wik1Reader <112945760+wik1Reader@users.noreply.github.com>
2024-01-16 12:28:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,68 @@
 # -*- coding: utf-8 -*
 from __future__ import absolute_import, unicode_literals
+
+# 필요한 모듈들을 임포트
 from datetime import datetime, timezone, timedelta
 import os
 import requests
 import pandas as pd
 import pywikibot
 from pywikibot import pagegenerators, Bot
-i1 = 0.965
+
+# 기본 임계값 설정
+i1 = 0.965 
 point = 0
+
+# 현재 연도와 월을 반환하는 함수
 def get_current_year_and_month():
-        now = datetime.now()
-        return now.year, now.month
+    now = datetime.now()
+    return now.year, now.month
 
+# 사용자가 익명(비회원, IP 기반) 유저인지 확인하는 함수
 def is_anonymous_user(self, username):
-  """
-  Check if the given user is an anonymous user (IP user).
-
-  :param username: The username to check.
-  :return: True if the user is anonymous, False otherwise.
-  """
   user = pywikibot.User(self.site, username)
   return user.isAnonymous()
 
-
+# 사용자가 관리자, 기록보호자, 점검자, 점검 면제자, 인터페이스 관리자, 장기 인증된 사용자  그룹인지 확인하는 함수
 def is_admin_user(username):
   site = pywikibot.Site()
-
   try:
     user = pywikibot.User(site, username)
-
     groups = user.groups()
-
-    admin_groups = {'sysop', 'Bureaucrat', 'Oversighter', 'CheckUser', 'Autopatrolled', 'Imterface administrator', 'extendconfirm'}
-    if any(group in admin_groups for group in groups):
-        return True
-    else:
-        return False
+    admin_groups = {'sysop', 'Bureaucrat', 'Oversighter', 'CheckUser', 'Autopatrolled', 'Interface administrator', 'extendedconfirmed'}
+    return any(group in admin_groups for group in groups)
   except pywikibot.NoUsername:
-      print(f"The username '{username}' does not exist.")
-      return False
+    print(f"The username '{username}' does not exist.")
+    return False
   except Exception as e:
-      print(f"An error occurred: {e}")
-      return False
+    print(f"An error occurred: {e}")
+    return False
 
+# 소스의 메인 로봇 클래스 정의
 class RespiceBOT(Bot):
 
-    """BOT que revierte desde ORES"""
-
+    # 초기화 함수, 여러 설정을 초기화
     def __init__(self, generator, site=None, **kwargs):
         super(RespiceBOT, self).__init__(**kwargs)
         self.available_options.update({
-            'gf': 0.085,
-            'dm': 0.970,
-            'wiki': 'kowiki'
+            'gf': 0.085, # Good faith 편집인지 판별하는 기준
+            'dm': 0.970, # 피해를 주는 편집인지 판별하는 기준
+            'wiki': 'kowiki' # 작업 대상 위키
         })
 
         self.generator = generator
         self.site = site
         if not self.site.logged_in():
             self.site.login()
-        self.wiki = "{}{}".format(self.site.lang, str(
-            self.site.family).replace('pedia', ''))
+        self.wiki = "{}{}".format(self.site.lang, str(self.site.family).replace('pedia', ''))
 
-    def run(self):
+    # 실제 실행 함수, 페이지를 필터링하고 리비전 위험을 검사
+    def run(self): 
         global point
-        global i1
         print('run')
         for page in filter(self.valid, self.generator):
             try:
-                revision, buena_fe, danina, resultado, algorithm = self.check_risk(
-                    page)
+                revision, buena_fe, danina, resultado, algorithm = self.check_risk(page)
             except Exception as exp:
                 print(exp)
                 continue
@@ -77,78 +70,62 @@ class RespiceBOT(Bot):
             if revision is None:
                 continue
             kst = timezone(timedelta(hours=9))
-            data = [revision, buena_fe, danina, resultado, page._rcinfo.get('user'), page.title(),
-            datetime.now(kst).strftime('%Y%m%d%H%M%S'), int(datetime.now(kst).timestamp()), algorithm]
+            data = [revision, buena_fe, danina, resultado, page._rcinfo.get('user'), page.title(), datetime.now(kst).strftime('%Y%m%d%H%M%S'), int(datetime.now(kst).timestamp()), algorithm]
             i=i1
             print(point)
+            # 익명사용자의 편집인 경우 임계값 조정
             if is_anonymous_user(self, page._rcinfo.get('user')):
                   i = 0.977
-            if is_admin_user(page._rcinfo.get('user')) == False:
+            # 그룹 검사, 정해져 있는 그룹이 아니면 로깅 수행
+            if not is_admin_user(page._rcinfo.get('user')):
                 self.do_log(data)
+                # 포인트가 임계값 이상이면 되돌리기 수행
                 if point >= i:
                   print(point)
-                  self.do_reverse(page, page._rcinfo.get('user'))
-                  if self.site.family.name == 'wikipedia' and self.site.lang == 'ko':
-                        ##self.check_user(page._rcinfo.get('user'), page.title())
-                        self.check_pagina(page.title())
+                  self.do_reverse(page, page._rcinfo.get('user')) 
 
+    # 페이지가 조건을 만족하는지 확인하는 필터 함수
     def valid(self, page):
         print('valid')
-        """
-        Check if we need to check the page from the LiveRCGenerator
-
-        @param page: Page to check
-        """
         return (
-            # Solo lo que sea edicion
-            page._rcinfo.get('type') == 'edit' and
-            # que no sea bot
-            not page._rcinfo.get('bot') and
-            # que este en el espacio principal o anexo
-            page._rcinfo.get('namespace') in {0, 104} and
-            # que no sea yo mismo
-            page._rcinfo.get('user') != self.site.username() and
-            # que no sea una reversa (tag de reversa, los RV manual no los considera)
-            'mw-rollback' not in list(page.revisions(total=10))[0]['tags']
+            page._rcinfo.get('type') == 'edit' and # 편집 유형
+            not page._rcinfo.get('bot') and # 봇이 아니어야 함
+            page._rcinfo.get('namespace') in {0, 104} and # 이름공간 체크(0:본문)
+            page._rcinfo.get('user') != self.site.username() and # 자기자신의 편집 체크
+            'mw-rollback' not in list(page.revisions(total=10))[0]['tags'] # 되돌리기 태그로 되돌린 편집이 아닌지 체크
         )
 
+    # 문서의 되돌림 위험성을 검사하는 함수
     def check_risk(self, page):
-        print('check_risk')
         global point
-        global i1
         i = i1
-        """Send a request to Wikimedia API to check the revert-risk of the page"""
         headers = {
             'User-Agent': 'RespiceBOT - an ORES/revertrisk-language-agnostic counter vandalism tool'}
         revision = page._rcinfo.get('revision')
         revision_check = str(revision.get('new'))
         url = 'https://api.wikimedia.org/service/lw/inference/v1/models/revertrisk-language-agnostic:predict'
 
+        # API 요청
         try:
-            data = requests.post(url=url, headers=headers, json={
-                                 "rev_id": revision_check, "lang": self.site.lang}).json()
+            data = requests.post(url=url, headers=headers, json={"rev_id": revision_check, "lang": self.site.lang}).json()
         except requests.RequestException as e:
             print(f"Error in API request: {e}")
-            # Handle the error or consider returning a default value
             return None, None, None, None, None
 
         if 'output' in data and 'probabilities' in data['output']:
             point = data['output']['probabilities']['true']
-            return revision_check, data['output']['probabilities']['false'], data['output']['probabilities']['true'], \
-                data['output']['probabilities']['true'] > i, 'revertrisk-language-agnostic'
+            return revision_check, data['output']['probabilities']['false'], data['output']['probabilities']['true'], data['output']['probabilities']['true'] > i, 'revertrisk-language-agnostic'
         else:
             print("Unexpected API response format")
-            # Handle the unexpected format or consider returning a default value
             return None, None, None, None, None
 
+    # 로그 기록 함수
     def do_log(self, data):
         global point
         i = 0.965
         wiki = self.wiki
-        general = os.path.join(os.path.dirname(os.path.realpath(
-            __file__)), "log", f"{wiki}-general.log")
-        positivo = os.path.join(os.path.dirname(os.path.realpath(
-            __file__)), "log", f"{wiki}-positivo.log")
+        general = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log", f"{wiki}-general.log")
+        positivo = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log", f"{wiki}-positivo.log")
         print('loging')
         with open(general, encoding="utf-8", mode="a+") as archivo:
             archivo.write("\t".join(map(str, data)) + "\n")
@@ -159,7 +136,32 @@ class RespiceBOT(Bot):
             with open(positivo, encoding="utf-8", mode="a+") as archivo:
                 archivo.write("\t".join(map(str, data)) + "\n")
 
-    """def check_user(self, user, page):
+    # 해당 페이지에 대해 관리가 필요한 경우 요청하는 함수
+    def check_pagina(self, pagina):
+        y365, m365  = get_current_year_and_month()
+
+        positivo = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log", f"{self.wiki}-positivo.log")
+        df_reversas = pd.read_csv(positivo, header=None, delimiter='\t')
+        page = df_reversas[5] == pagina
+
+        tabp.text += "\n" + tpl
+        try:
+            tabp.save(summary=u'[[{0}]] 문서를 보호 요청함'.format(pagina))
+        except:
+            pass
+
+    # 되돌리기를 실행하는 함수
+    def do_reverse(self, page, user):
+        try:
+            print('reversa de ' + page.title())
+            self.site.rollbackpage(page, user=user)
+        except Exception as exp:
+            print(exp)
+            pass
+
+
+""" 
+      def check_user(self, user, page):
         # Check for consecutive reversions by the same user
         wiki = self.wiki
         positive_log_path = os.path.join(os.path.dirname(
@@ -168,6 +170,7 @@ class RespiceBOT(Bot):
             positive_log_path, header=None, delimiter='\t')
         user_reversions = df_reversas[(
             df_reversas[4] == user) & (df_reversas[5] == page)]
+
 
         # Handle two consecutive reversions by a registered user
          if len(user_reversions) == 2:
@@ -180,6 +183,7 @@ class RespiceBOT(Bot):
                 except pywikibot.Error as e:
                     print(f"Error saving talk page: {e}")
                 return
+
 
         # Handle four consecutive reversions by any user
         if len(user_reversions) == 4:
@@ -199,51 +203,14 @@ class RespiceBOT(Bot):
                 vandalism_page.save(summary=summary)
             except pywikibot.Error as e:
                 print(f"Error saving vandalism page: {e}")
-        return """
-        #토론이 필요한 사항
+        return 
+"""
+#토론이 필요한 사항
+# 현재 주석 처리되어 있음. 필요시 주석을 해제하고 사용하면 됨 
 
 
-    def check_pagina(self, pagina):
-        y365, m365  = get_current_year_and_month()
-        wiki = self.wiki
-        positivo = "{0}/log/{1}-positivo.log".format(
-            os.path.dirname(os.path.realpath(__file__)), wiki)
-        df_reversas = pd.read_csv(positivo, header=None, delimiter='\t')
-        page = df_reversas[5] == pagina
-        past = (int(datetime.utcnow().timestamp()) -
-                df_reversas[7]) < (60 * 60 * 2)  # 4 horas
-        users = df_reversas[page & past][4].nunique()
-        rows = df_reversas[page & past]
-        if len(rows) < 6 or users < 2:
-            return
-
-        tabp = pywikibot.Page(
-            self.site, title='문서 관리 요청/'+y365+'년'+m365+'월',
-            ns=4)
-        if tabp.get().find('{{{{a|{0}}}}}'.format(pagina)) != -1:
-            return
-        tpl = "\n" + \
-            '{{{{풀기:사:RespicBOT/TABP|페이지={0}|서명=~~~~}}}}'.format(
-                pagina)
-        tabp.text += "\n" + tpl
-        try:
-            tabp.save(
-                summary=u'[[{0}]] 문서를 보호 요청함'.format(pagina))
-        except:
-            pass
-        return
-
-    def do_reverse(self, page, user):
-        try:
-            print('reversa de ' + page.title())
-
-            self.site.rollbackpage(page, user=user)
-        except Exception as exp:
-            print(exp)
-            pass
-
-
-def main(*args):  
+# 메인 함수, 스크립트 실행 시 초기 설정을 읽고 로봇을 실행
+def main(*args):
     print('main')
     opts = {}
     local_args = pywikibot.handle_args(args)
@@ -264,7 +231,7 @@ def main(*args):
     bot = RespiceBOT(pagegenerators.LiveRCPageGenerator(site), site=site, **opts)
     bot.run()
 
-
+# 스크립트 진입점
 if __name__ == '__main__':
     print('start')
     main()
