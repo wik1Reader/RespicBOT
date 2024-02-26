eb215124abd6a64432a34ba8f3fc6a81803eef38
wik1Reader <112945760+wik1Reader@users.noreply.github.com>
2024-02-26 22:08:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ class RespiceBOT(Bot):
     def __init__(self, generator, site=None, **kwargs):
         super(RespiceBOT, self).__init__(**kwargs)
         self.available_options.update({
-            'gf': 0.085, # Good faith 편집인지 판별하는 기준
-            'dm': 0.970, # 피해를 주는 편집인지 판별하는 기준
+            'gf': 0.585, # Good faith 편집인지 판별하는 기준
+            'dm': 0.978, # 피해를 주는 편집인지 판별하는 기준
             'wiki': 'kowiki' # 작업 대상 위키
         })
 
@@ -59,6 +59,8 @@ class RespiceBOT(Bot):
     # 실제 실행 함수, 페이지를 필터링하고 리비전 위험을 검사
     def run(self): 
         global point
+        global i1
+        i = 0
         print('run')
         for page in filter(self.valid, self.generator):
             try:
@@ -71,18 +73,15 @@ class RespiceBOT(Bot):
                 continue
             kst = timezone(timedelta(hours=9))
             data = [revision, buena_fe, danina, resultado, page._rcinfo.get('user'), page.title(), datetime.now(kst).strftime('%Y%m%d%H%M%S'), int(datetime.now(kst).timestamp()), algorithm]
-            i=i1
-            print(point)
             # 익명사용자의 편집인 경우 임계값 조정
             if is_anonymous_user(self, page._rcinfo.get('user')):
                   i = 0.977
             # 그룹 검사, 정해져 있는 그룹이 아니면 로깅 수행
             if not is_admin_user(page._rcinfo.get('user')):
-                self.do_log(data)
                 # 포인트가 임계값 이상이면 되돌리기 수행
                 if point >= i:
                   print(point)
-                  self.do_reverse(page, page._rcinfo.get('user')) 
+                  self.do_reverse(self, data) 
 
     # 페이지가 조건을 만족하는지 확인하는 필터 함수
     def valid(self, page):
@@ -104,7 +103,7 @@ class RespiceBOT(Bot):
         revision = page._rcinfo.get('revision')
         revision_check = str(revision.get('new'))
         url = 'https://api.wikimedia.org/service/lw/inference/v1/models/revertrisk-language-agnostic:predict'
-        
+
         # API 요청
         try:
             data = requests.post(url=url, headers=headers, json={"rev_id": revision_check, "lang": self.site.lang}).json()
@@ -122,97 +121,18 @@ class RespiceBOT(Bot):
             print("Unexpected API response format")
             return None, None, None, None, None
 
-    # 로그 기록 함수
-    def do_log(self, data):
-        global point
-        i = 0.965
-        wiki = self.wiki
-        general = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log", f"{wiki}-general.log")
-        positivo = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log", f"{wiki}-positivo.log")
-        print('loging')
-        with open(general, encoding="utf-8", mode="a+") as archivo:
-            archivo.write("\t".join(map(str, data)) + "\n")
-        if is_anonymous_user(self, data[4]):
-            i = 0.977
-
-        if point > i:
-            with open(positivo, encoding="utf-8", mode="a+") as archivo:
-                archivo.write("\t".join(map(str, data)) + "\n")
-
-    # 해당 페이지에 대해 관리가 필요한 경우 요청하는 함수
-    def check_pagina(self, pagina):
-        y365, m365  = get_current_year_and_month()
-
-        positivo = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log", f"{self.wiki}-positivo.log")
-        df_reversas = pd.read_csv(positivo, header=None, delimiter='\t')
-        page = df_reversas[5] == pagina
-
-        tabp.text += "\n" + tpl
+    def do_reverse(self, page, data):
         try:
-            tabp.save(summary=u'[[{0}]] 문서를 보호 요청함'.format(pagina))
-        except:
-            pass
-
-    # 되돌리기를 실행하는 함수
-    def do_reverse(self, page, user):
-        try:
-            print('reversa de ' + page.title())
-            self.site.rollbackpage(page, user=user)
+            print("log_2")
+            page_1 = pywikibot.Page(self.site, title="user:RespiceBOT/ture_log", ns=3)
+            page_1.text += "\t"+"[[특:차이/"+str(data[0])+"|"+str(data[0])+"]]"+"\t"+str(data[1])+"\t"+str(data[2])+"\t"+str(data[3])+"\t"+"[[특:기여/"+str(data[4])+"]]"+"\t"+"[["+str(data[5])+"]]"+"\t"+str(data[6])+"\t"+str(data[7])+"<br>"
+            summary = "TEST"
+            page_1.save(summary=summary)
         except Exception as exp:
             print(exp)
             pass
 
-
-""" 
-      def check_user(self, user, page):
-        # Check for consecutive reversions by the same user
-        wiki = self.wiki
-        positive_log_path = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), 'log', f"{wiki}-positivo.log")
-        df_reversas = pd.read_csv(
-            positive_log_path, header=None, delimiter='\t')
-        user_reversions = df_reversas[(
-            df_reversas[4] == user) & (df_reversas[5] == page)]
-
-
-        # Handle two consecutive reversions by a registered user
-         if len(user_reversions) == 2:
-            if not pywikibot.User(self.site, user).isAnonymous():
-                talk_page = pywikibot.Page(self.site, title=user, ns=3)
-                talk_page.text += u"\n{{subst:Aviso prueba2|" + page + "}} ~~~~"
-                summary = u'Aviso de pruebas a usuario tras reversiones consecutivas'
-                try:
-                    talk_page.save(summary=summary)
-                except pywikibot.Error as e:
-                    print(f"Error saving talk page: {e}")
-                return
-
-
-        # Handle four consecutive reversions by any user
-        if len(user_reversions) == 4:
-            vandalism_page = pywikibot.Page(
-                self.site, title='Vandalismo en curso', ns=2)
-            template = "\n" + u'{{subst:'
-            template += 'ReportevandalismoIP' if pywikibot.User(
-                self.site, user).isAnonymous() else 'Reportevandalismo'
-            template += u'|1=' + user
-            template += u'|2=Reversiones: ' + (', '.join(
-                map(lambda x: u'[[Special:Diff/' + str(x) + '|diff: ' + str(x) + ']]', user_reversions[0])))
-            template += u'}}'
-            vandalism_page.text += "\n" + template
-            summary = u'Reportando al usuario [[Special:Contributions/' + \
-                user + '|' + user + ']] por posibles ediciones vándalicas'
-            try:
-                vandalism_page.save(summary=summary)
-            except pywikibot.Error as e:
-                print(f"Error saving vandalism page: {e}")
-        return 
-"""
-#토론이 필요한 사항
-# 현재 주석 처리되어 있음. 필요시 주석을 해제하고 사용하면 됨 
-
-
-# 메인 함수, 스크립트 실행 시 초기 설정을 읽고 로봇을 실행
+    
 def main(*args):
     print('main')
     opts = {}
