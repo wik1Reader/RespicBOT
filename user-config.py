8cf113b650e920993018d5c8e263451582ab012b
wik1Reader <112945760+wik1Reader@users.noreply.github.com>
2024-02-28 08:32:46 +0900

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -1,5 +1,4 @@
 usernames['wikipedia']['ko'] = 'RespiceBOT'
 family = 'wikipedia'
 mylang = 'ko'
-simulate = 'ture'
 authenticate [ '*.wikipedia.org' ]  =  ( '<consumer_key>' , '<consumer_secret>' ,  '<access_key>' ,  '<access_secret>' )
