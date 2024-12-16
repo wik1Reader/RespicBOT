5745d1cd20c4460b955974f426f11f9b92533dd7
wik1Reader <112945760+wik1Reader@users.noreply.github.com>
2024-12-16 19:36:36 +0900

    Delete user-config.py

--- user-config.py
+++
@@ -1,4 +0,0 @@
-usernames['wikipedia']['ko'] = 'RespiceBOT'
-family = 'wikipedia'
-mylang = 'ko'
-authenticate['ko.wikipedia.org'] = ('<consumer_key>', '<consumer_secret>', '<access_token>', '<access_secret>')
